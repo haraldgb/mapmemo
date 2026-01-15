13f4dd34f26a9de877873ef70db3880dae79f427
Harald <haraldgb@gmail.com>
2026-01-15 14:22:11 +0800

    config: prettier(apply)

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import importPlugin from 'eslint-plugin-import'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -15,6 +16,7 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      eslintConfigPrettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -25,6 +27,12 @@ export default defineConfig([
     },
     rules: {
       'import/no-default-export': 'error',
+      'no-restricted-imports': [
+        'error',
+        {
+          paths: [],
+        },
+      ],
     },
   },
 ])
--- frontend/src/mapMemo/MapMemo.tsx
+++ frontend/src/mapMemo/MapMemo.tsx
@@ -1,7 +1,7 @@
-import { useSelector } from "react-redux";
-import type { RootState } from "../store";
+import { useSelector } from 'react-redux'
+import type { RootState } from '../store'
 
 export const MapMemo = () => {
-  const message = useSelector((state: RootState) => state.mapmemo.message);
-  return <h1>{message}</h1>;
-};
+  const message = useSelector((state: RootState) => state.mapmemo.message)
+  return <h1>{message}</h1>
+}
