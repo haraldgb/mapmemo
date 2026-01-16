4d92168e2e1ffc3e9a57272cea28eca3170ca557
Harald <haraldgb@gmail.com>
2026-01-16 17:45:51 +0800

    config: allow TODO comments
    AI gets hung up on fixing all linting, even though I'd like to keep
    the warning myself.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -29,7 +29,7 @@ export default defineConfig([
       'import/no-default-export': 'error',
       'brace-style': ['error', '1tbs', { allowSingleLine: false }],
       curly: ['error', 'all'],
-      'no-warning-comments': ['warn', { terms: ['TODO'], location: 'start' }],
+      // 'no-warning-comments': ['warn', { terms: ['TODO'], location: 'start' }],
       'no-unreachable': 'warn',
       'no-console': 'error',
       'no-restricted-syntax': [
