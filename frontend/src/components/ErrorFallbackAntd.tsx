524fb629212e3dad6d4db96675ab749891d54442
Harald <haraldgb@gmail.com>
2026-01-18 13:29:06 +0800

    feat: wrap app with error boundary

---
+++ frontend/src/components/ErrorFallbackAntd.tsx
@@ -0,0 +1,27 @@
+import { Result, Typography } from 'antd'
+import type { FallbackProps } from 'react-error-boundary'
+
+const { Paragraph, Text } = Typography
+
+/**
+ * Placeholder fallback component until implementing comprehensive styling.
+ */
+export const ErrorFallbackAntd = ({ error }: FallbackProps) => {
+  const errorMessage =
+    error instanceof Error ? error.message : 'error is not an instance of Error'
+  const errorStack = error instanceof Error ? error.stack : undefined
+
+  return (
+    <Result
+      status="error"
+      title="Something went wrong"
+      subTitle={errorMessage}
+    >
+      {errorStack ? (
+        <Paragraph>
+          <Text code>{errorStack}</Text>
+        </Paragraph>
+      ) : null}
+    </Result>
+  )
+}
